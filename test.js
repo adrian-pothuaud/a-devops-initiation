41d5cc06b3be5353b59483e6127a447e3b68be2b
Adrian Pothuaud <adrianpothuaud@gmail.com>
2018-02-02 06:27:05 +0000

    add a new test

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 describe("My feature", function() {
     describe("subfeature 1", function() {
         it("does one thing", function() {
@@ -14,4 +16,12 @@ describe("My feature", function() {
         it("does another thing", function() {
         });
     });
+    
+    // changement à valider (test 1)
+    describe("subfeature 3", function() {
+       it("will fail", function() {
+           assert(false);
+       });
+    });
+    // commenter pour passer (test 2)
 });

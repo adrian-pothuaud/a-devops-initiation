a1f4e380f601f165d6c7ab8ed5c2ee0bf3b1a6b8
Adrian Pothuaud <adrianpothuaud@gmail.com>
2018-02-03 07:03:03 +0000

    add deployment and security infos on home page

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static(path.join(__dirname, 'mochawesome-report/assets')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/tests', (req, res) => res.sendFile(path.join(__dirname + '/mochawesome-report/mochawesome.html')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))

5a0a36fa3a656fd50de9d932c35edd69cb911942
Adrian Pothuaud <adrianpothuaud@gmail.com>
2018-02-03 07:48:43 +0000

    cancelled e2e tests

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,6 @@ var gulp = require('gulp');
 
 gulp.task('one', function(done) {
   // do stuff
-  console.log("Moving last report to public repo");
-  gulp
-    .src("mochawesome-report/**.*")
-    .pipe(gulp.dest('public/mochawesome-report/'));
-  gulp
-    .src("mochawesome-report/assets/**.*")
-    .pipe(gulp.dest('public/mochawesome-report/assets/'));
   done();
 });
 

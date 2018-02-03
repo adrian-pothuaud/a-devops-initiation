ef864f83c755341a85f718a818c70fbdad0053e6
Adrian Pothuaud <adrianpothuaud@gmail.com>
2018-02-03 07:38:26 +0000

    add end to end tests sample

--- end-to-end-tests/home-page.js
+++ end-to-end-tests/home-page.js
@@ -1,16 +1,12 @@
-module.exports = {
-  'Demo test Google' : function (client) {
-    client
-      .url('http://www.google.com')
-      .waitForElementVisible('body', 1000)
-      .assert.title('Google')
-      .assert.visible('input[type=text]')
-      .setValue('input[type=text]', 'rembrandt van rijn')
-      .waitForElementVisible('button[name=btnG]', 1000)
-      .click('button[name=btnG]')
-      .pause(1000)
-      .assert.containsText('ol#rso li:first-child',
-        'Rembrandt - Wikipedia')
-      .end()
-  }
-}
+var casper = require('casper').create();
+casper.start('http://casperjs.org/');
+
+casper.then(function() {
+    this.echo('First Page: ' + this.getTitle());
+});
+
+casper.thenOpen('http://phantomjs.org', function() {
+    this.echo('Second Page: ' + this.getTitle());
+});
+
+casper.run();

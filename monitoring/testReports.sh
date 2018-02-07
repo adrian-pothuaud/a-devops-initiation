b7ef399f5b947f8978ea898389871fafa7cbd910
Adrian Pothuaud <adrianpothuaud@gmail.com>
2018-02-07 19:53:42 +0000

    fixed requests monitoring + navigation links

---
+++ monitoring/testReports.sh
@@ -0,0 +1,8 @@
+rm -rf public/assets/*
+rm -rf public/assets/
+rm -rf public/*.html
+rm -rf public/*.json
+
+mv mochawesome-report/* public/
+rm -rf mochawesome-report/*
+rm -rf mochawesome-report
---
+++ test/index.js
@@ -0,0 +1,29 @@
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../index');
+let should = chai.should();
+
+chai.use(chaiHttp);
+//Our parent block
+describe('Pages', () => {
+
+/*
+  * Test the /GET route
+  */
+  describe('/GET index', () => {
+      it('it should GET status code 200', (done) => {
+        chai.request(server)
+            .get('/')
+            .end((err, res) => {
+                if (err) throw err;
+                res.should.have.status(200);
+              done();
+            });
+      });
+  });
+
+});

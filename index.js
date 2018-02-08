bf64536f2141e479bc30696df2ed9e71b7d7fe3a
Adrian Pothuaud <adrianpothuaud@gmail.com>
2018-02-08 09:59:34 +0000

    push monitoring

--- index.js
+++ index.js
@@ -104,7 +104,55 @@ var app = express();
         })
         
         .get('/pushs', (req, res) => {
-            res.render('pages/pushs');
+            
+            var cr = new clientRequest({
+                page:  "Pushs",
+                client: req.clientIp,
+                status: req.status,
+                date: new Date(),
+                isTest: false
+            });
+            cr.save(function(err, result) {
+                if (err) throw err;
+                
+                codePush.aggregate(
+                	[{
+                		$group:{
+                			_id: { $dateToString: { format: "%m,%d %Hh", date: "$date" }},
+                			count: { $sum: 1 }
+                		}
+                	},{
+                	    $sort: { _id : 1 }
+                	}], (err, result) => {
+                        if (err) throw err;
+                        
+                        var finalDates = [];
+                        var finalCpts = [];
+                        
+                        for(let resu of result){
+                            finalDates.push(resu["_id"]);
+                            finalCpts.push(resu["count"]);
+                        }
+                        
+                        var plotly = require('plotly')('adrianpothuaud', 'OWRADETaAnxEYCmnrIlf');
+
+                        var data = [
+                          {
+                            x: finalDates,
+                            y: finalCpts,
+                            type: "scatter"
+                          }
+                        ];
+                        
+                        var graphOptions = {filename: "pushs", fileopt: "overwrite"};
+                        
+                        plotly.plot(data, graphOptions, function (err, msg) {
+                            if(err) throw err;
+                            res.render('pages/pushs');
+                        });
+                    }
+                );
+            });
         })
         
         .post('/pushs', (req, res) => {
@@ -116,7 +164,55 @@ var app = express();
         })
         
         .get('/deployments', (req, res) => {
-            res.render('pages/deployments');
+            
+            var cr = new clientRequest({
+                page:  "Deployments",
+                client: req.clientIp,
+                status: req.status,
+                date: new Date(),
+                isTest: false
+            });
+            cr.save(function(err, result) {
+                if (err) throw err;
+                
+                deploy.aggregate(
+                	[{
+                		$group:{
+                			_id: { $dateToString: { format: "%m,%d %Hh", date: "$date" }},
+                			count: { $sum: 1 }
+                		}
+                	},{
+                	    $sort: { _id : 1 }
+                	}], (err, result) => {
+                        if (err) throw err;
+                        
+                        var finalDates = [];
+                        var finalCpts = [];
+                        
+                        for(let resu of result){
+                            finalDates.push(resu["_id"]);
+                            finalCpts.push(resu["count"]);
+                        }
+                        
+                        var plotly = require('plotly')('adrianpothuaud', 'OWRADETaAnxEYCmnrIlf');
+
+                        var data = [
+                          {
+                            x: finalDates,
+                            y: finalCpts,
+                            type: "scatter"
+                          }
+                        ];
+                        
+                        var graphOptions = {filename: "deploys", fileopt: "overwrite"};
+                        
+                        plotly.plot(data, graphOptions, function (err, msg) {
+                            if(err) throw err;
+                            res.render('pages/deployments');
+                        });
+                    }
+                );
+            });
         })
         
         .post('/deployments', (req, res) => {
